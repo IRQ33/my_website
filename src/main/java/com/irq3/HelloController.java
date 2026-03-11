2f62892c2314c5d27313491ca0546f57272abf1b
irq3 <noreply_irq@gmail.com>
2026-03-11 16:09:41 +0100

    Initial commit

---
+++ src/main/java/com/irq3/HelloController.java
@@ -0,0 +1,14 @@
+package com.irq3;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/")
+public class HelloController {
+
+    @Get(produces = MediaType.TEXT_HTML)
+    public String heyo(){
+        return "Heyo";
+    }
+}
